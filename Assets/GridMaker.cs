f164862c2cc086366e0c6bf1f2518ca99aae49a4
Ethan Gonzalez <112298070+EthanGon@users.noreply.github.com>
2026-01-03 16:03:38 -0500

    Setting up grid spawner
    I'm not sure if I should do it this way, but it was the first idea that came to mind, and I want to see if I can do it

---
+++ Assets/GridMaker.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GridMaker : MonoBehaviour
+{
+    public GameObject pixel;
+    public Color[] colors;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
