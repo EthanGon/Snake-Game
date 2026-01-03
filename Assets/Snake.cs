0007816f7410419ad5c54f862021d61e6b6c2b70
Ethan Gonzalez <112298070+EthanGon@users.noreply.github.com>
2026-01-03 17:54:29 -0500

    Snake movement works (WIP Still)
    Got movement to work, so when I press a direction, it sets the snakes direction and delays how often the snakes move.

--- Assets/Snake.cs
+++ Assets/Snake.cs
@@ -1,11 +1,18 @@
+using System.Collections;
 using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
+    public Vector3 dir;
+    private Rigidbody2D rb;
+    public float timer;
+    public float interval;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        dir = Vector2.zero;
     }
 
     // Update is called once per frame
@@ -13,8 +20,42 @@ public class Snake : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.position = new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z);
-            Debug.Log("Pressed D");
+            dir = Vector2.right * .5f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            dir = Vector2.left * .5f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            dir = Vector2.up * .5f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            dir = Vector2.down * .5f;
+        }
+
+        
+
+        if (timer < interval && dir != Vector3.zero)
+        {
+            timer = timer + Time.deltaTime;
+        } 
+        else
+        {
+            transform.position = new Vector3(transform.position.x + dir.x, transform.position.y + dir.y, 0.0f);
+            timer = 0.0f;
         }
     }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+   
+
 }
