76826fb1e4f60e25ad7838145ad35d70d234ceed
Ethan Gonzalez <112298070+EthanGon@users.noreply.github.com>
2026-01-03 17:01:55 -0500

    Setting up snake movement

---
+++ Assets/Snake.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z);
+            Debug.Log("Pressed D");
+        }
+    }
+}
