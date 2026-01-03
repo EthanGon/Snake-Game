60557f168d4fb20758f5c4ed53c2bfce723f0986
Ethan Gonzalez <112298070+EthanGon@users.noreply.github.com>
2026-01-03 16:46:36 -0500

    Setting up predefined grid.
    Easier to setup, and extra stuff not needed

--- Assets/GridMaker.cs
+++ Assets/GridMaker.cs
@@ -5,15 +5,23 @@ public class GridMaker : MonoBehaviour
     public GameObject pixel;
     public Color[] colors;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    // Update is called once per frame
     void Start()
     {
-        
-    }
+        int colorToPick = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (float y = 4; y >= -4; y-=.5f) 
+        { 
+            for (float x = -4; x <= 4; x+=.5f)
+            {
+                Vector3 pos = new Vector3(x, y, 0.0f);
+                GameObject newPixel = Instantiate(pixel, pos, Quaternion.identity);
+                newPixel.GetComponent<SpriteRenderer>().color = colors[colorToPick % colors.Length];
+
+                colorToPick++;
+            }
+        }
+
     }
 }
